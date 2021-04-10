8633de68d031e17fe6db1f26aabb3e716e7d5f79
Josh Motta <joshua.a.motta@gmail.com>
2021-04-10 16:10:18 -0400

    type numbers complete

--- beginner-javascript/playground/types.js
+++ beginner-javascript/playground/types.js
@@ -1,22 +1,48 @@
-const name = 'Joshua';
-const middle = "Anthony";
-const last = `Motta`;
+/* Strings */
 
-const sentence = `she's so "cool"`
+// const name = 'Joshua';
+// const middle = "Anthony";
+// const last = `Motta`;
 
-const hello = `Hello my name is ` + name + `. Nice to meet you.`;
-const hello2 = `Hello my name is ${name}. Nice to meet you. I am ${1 + 100} years old.`
+// const sentence = `she's so "cool"`
 
-const html = `
-    <div>
-        <h2>${name}</h2>
-        <p>${hello}</p>
-    </div>  
-`;
+// const hello = `Hello my name is ` + name + `. Nice to meet you.`;
+// const hello2 = `Hello my name is ${name}. Nice to meet you. I am ${1 + 100} years old.`
 
-document.body.innerHTML = html;
+// const html = `
+//     <div>
+//         <h2>${name}</h2>
+//         <p>${hello}</p>
+//     </div>  
+// `;
+
+// document.body.innerHTML = html;
+
+
+/* Numbers */
+
+const age = 100.5;
+const name = 'Josh';
+
+const a = 10;
+const b = 20;
+
+const smarties = 20;
+const kids = 3;
+const eachKidGets = smarties / kids;
+console.log(`Each kid gets ${Math.floor(eachKidGets)}.`);
+console.log(`Dad gets ${smarties % kids}.`);
+
+const price = 1034;
+
 
 /* Additional Notes */
 // back ticks keep spacing
 // concatenation when 2 strings are combined
 // interpolation when a variable is used within a string
+
+// Common Math Functions
+// round, floor, ceil, random
+
+// NaN - Not a number is a number
+// Infinity and -Infinity are both numbers
