7ce3217624efd729684d78b5be47d6b69b7d70c2
Josh Motta <joshua.a.motta@gmail.com>
2021-04-13 21:00:05 -0400

    Finished debugging section

---
+++ beginner-javascript/exercises/16 - Debugging/debugging.js
@@ -0,0 +1,64 @@
+const people = [
+  { name: 'Wes', cool: true, country: 'Canada' },
+  { name: 'Scott', cool: true, country: 'Merica' },
+  { name: 'Snickers', cool: false, country: 'Dog Country' },
+];
+
+people.forEach((person, index) => {
+  // debugger;
+  console.log(person.name);
+});
+
+// Console Methods
+// warn, error, table, count, group and groupEnd, groupCollapsed
+
+// Callstack
+// Read and understand the callstack, stack trace
+
+// Grabbing Elements
+// $0 to check last element clicked. Increment for prior elements.
+
+// Breakpoints
+// debugger or set breakpoints
+
+// Scope
+
+// Network Requests
+
+// Break On Attribute
+
+// Some Setup Code
+
+function doctorize(name) {
+  // console.count(`running Doctorize for ${name}`);
+  return `Dr. ${name}`;
+}
+
+function greet(name) {
+  doesntExist(); // Cause an error
+  return `Hello ${name}`;
+}
+
+function go() {
+  const name = doctorize(greet('Wes'));
+  console.log(name);
+}
+
+const button = document.querySelector('.bigger');
+button.addEventListener('click', function (e) {
+  const newFontSize =
+    parseFloat(getComputedStyle(e.currentTarget).fontSize) + 1;
+  e.currentTarget.style.fontSize = `${newFontSize}px`;
+});
+
+// A Dad joke fetch
+async function fetchDadJoke() {
+  const res = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      Accept: 'text/plain',
+    },
+  });
+  const joke = await res.text();
+  console.log(joke);
+  return joke;
+}
