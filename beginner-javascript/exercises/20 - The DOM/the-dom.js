74011ee2672cdebbbdf072349bc1578058b223ef
Josh Motta <joshua.a.motta@gmail.com>
2021-04-19 13:35:30 -0400

    Completed element properties/methods section

--- beginner-javascript/exercises/20 - The DOM/the-dom.js
+++ beginner-javascript/exercises/20 - The DOM/the-dom.js
@@ -2,4 +2,21 @@ const p = document.querySelector('p');
 const imgs = document.querySelectorAll('.item img');
 const item2 = document.querySelector('.item2');
 const item2Img = item2.querySelector('img');
-console.log(item2);
+const heading = document.querySelector('h2');
+
+console.dir(heading.textContent);
+console.dir(heading.innerText);
+// set the textContent property on that element
+// heading.textContent = 'Josh is cool';
+// console.dir(heading.textContent);
+// console.log(heading.innerText);
+
+console.log(heading.innerHTML);
+console.log(heading.outerHTML);
+
+const pizzaList = document.querySelector('.pizza');
+console.log(pizzaList.textContent);
+
+// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+pizzaList.insertAdjacentText('afterbegin', '🍕 ');
+pizzaList.insertAdjacentText('beforeend', '🍕');
