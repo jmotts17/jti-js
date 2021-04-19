b0939c331d9ea1abe5d15dbe41e8c65fd565e40e
Josh Motta <joshua.a.motta@gmail.com>
2021-04-19 13:48:04 -0400

    Completed classes section

--- beginner-javascript/exercises/20 - The DOM/the-dom.js
+++ beginner-javascript/exercises/20 - The DOM/the-dom.js
@@ -1,22 +1,35 @@
-const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.item img');
-const item2 = document.querySelector('.item2');
-const item2Img = item2.querySelector('img');
-const heading = document.querySelector('h2');
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
+// const item2 = document.querySelector('.item2');
+// const item2Img = item2.querySelector('img');
+// const heading = document.querySelector('h2');
 
-console.dir(heading.textContent);
-console.dir(heading.innerText);
-// set the textContent property on that element
-// heading.textContent = 'Josh is cool';
 // console.dir(heading.textContent);
-// console.log(heading.innerText);
+// console.dir(heading.innerText);
+// // set the textContent property on that element
+// // heading.textContent = 'Josh is cool';
+// // console.dir(heading.textContent);
+// // console.log(heading.innerText);
 
-console.log(heading.innerHTML);
-console.log(heading.outerHTML);
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML);
 
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent);
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
 
-// pizzaList.textContent = `${pizzaList.textContent} 🍕`;
-pizzaList.insertAdjacentText('afterbegin', '🍕 ');
-pizzaList.insertAdjacentText('beforeend', '🍕');
+// // pizzaList.textContent = `${pizzaList.textContent} 🍕`;
+// pizzaList.insertAdjacentText('afterbegin', '🍕 ');
+// pizzaList.insertAdjacentText('beforeend', '🍕');
+
+// Classes!
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.remove('cool');
+
+console.log(pic.classList);
+
+function toggleRound() {
+  pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
