b2739e6bb33e1caad27e9a4c5de9a5d42f501b6e
Josh Motta <joshua.a.motta@gmail.com>
2021-04-26 11:35:14 -0400

    Finished targets, bubbling, prop, capture

--- beginner-javascript/exercises/29 - Events/events.js
+++ beginner-javascript/exercises/29 - Events/events.js
@@ -18,23 +18,61 @@ coolButton.addEventListener('click', hooray);
 // Listen on multiple items
 const buyButtons = document.querySelectorAll('button.buy');
 
-function buyItem() {
-  console.log('Buying item');
+// function buyItem() {
+//   console.log('Buying item');
+// }
+
+// function handleBuyButtonClick(buyButton) {
+//   console.log('Binding the buy button');
+//   buyButton.addEventListener('click', buyItem);
+// }
+
+// buyButtons.forEach(handleBuyButtonClick);
+
+// buyButtons.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     console.log('You clicked it!');
+//   });
+// });
+
+function handleBuyButtonClick(event) {
+  console.log('You clicked a button');
+  const button = event.target;
+  //   console.log(button.textContent);
+  //   console.log(parseFloat(event.target.dataset.price));
+  console.log(event.target);
+  console.log(event.currentTarget);
+  console.log(event.target === event.currentTarget);
+  // Stop this event from bubbling up
+  //   event.stopPropagation();
 }
 
-function handleBuyButtonClick(buyButton) {
-  console.log('Binding the buy button');
-  buyButton.addEventListener('click', buyItem);
-}
+buyButtons.forEach(function (buyButton) {
+  buyButton.addEventListener('click', handleBuyButtonClick);
+});
 
-buyButtons.forEach(handleBuyButtonClick);
+window.addEventListener(
+  'click',
+  function (event) {
+    console.log('You clicked the window:');
+    console.log(event.target);
+    console.log(event.type);
+    // event.stopPropagation();
+    console.log(event.bubbles);
+  },
+  {
+    capture: true,
+  }
+);
 
-buyButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    console.log('You clicked it!');
-  });
+const photoEl = document.querySelector('.photo');
+photoEl.addEventListener('mousemove', function (e) {
+  console.log(e.currentTarget);
+  console.log(this);
 });
 
 /* Additional Notes */
 // Callback Function - A function that we pass to a method that will be called at a later time.
 // Anonymous functions can't be removed
+// event.target is the element that actually got clicked
+// event.currentTarget is the actual element
